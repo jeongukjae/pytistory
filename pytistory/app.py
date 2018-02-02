1c40a182c76177b5eb7041d149b63a429c62bb02
JeongUkJae <JeongUkJae@gmail.com>
2018-02-03 00:48:21 +0900

    Headless 브라우저 인증 추가

--- pytistory/app.py
+++ pytistory/app.py
@@ -7,6 +7,8 @@ import multiprocessing
 import webbrowser
 import time
 
+import requests
+
 from .api import Blog
 from .exceptions import ConfigurationError
 from .callback import CallbackServer
@@ -19,6 +21,8 @@ CONFIG_SECTION_NAME = 'pytistory'
 CONFIG_CLIENT_ID = 'client_id'
 CONFIG_SECRET_KEY = 'secret_key'
 CONFIG_ACCESS_TOKEN = 'access_token'
+CONFIG_TISTORY_ID = 'tistory_id'
+CONFIG_TISTORY_PASSWORD = 'tistory_password'
 
 def callback_process(namespace, event):
     """flask가 실행되는 process 함수.
@@ -46,10 +50,12 @@ class PyTistory:
         self.client_id = ''
         self.secret_key = ''
         self.access_token = ''
+        self.tistory_id = ''
+        self.tistory_password = ''
 
         self.blog = None
 
-    def _read_configuration_file(self):
+    def _read_configuration_file(self, headless_auth=False):
         """Configuration File을 읽고, ConfigParser를 반환합니다.
 
         self.file_name에 해당하는 파일을 읽고, 해당 파일의 설정값들을 반환합니다.
@@ -75,10 +81,26 @@ class PyTistory:
         if CONFIG_SECRET_KEY not in config[CONFIG_SECTION_NAME]:
             raise ConfigurationError('Cannot find a tistory secret key in `{}`.'\
                 .format(self.file_name))
+        if headless_auth:
+            if CONFIG_TISTORY_ID not in config[CONFIG_SECTION_NAME]:
+                raise ConfigurationError('Cannot find a tistory user id in `{}`.'\
+                    .format(self.file_name))
+            if CONFIG_TISTORY_PASSWORD not in config[CONFIG_SECTION_NAME]:
+                raise ConfigurationError('Cannot find a tistory password in `{}`.'\
+                    .format(self.file_name))
 
         return config
 
-    def _set_access_token(self):
+    @staticmethod
+    def _is_listening():
+        try:
+            requests.get('http://localhost:5000/')
+        except requests.exceptions.ConnectionError:
+            return False
+
+        return True
+
+    def _set_access_token(self, headless_auth):
         """Tistory의 access_token을 설정하는 함수입니다.
 
         `CallbackServer`를 구동시킨 후, 티스토리 인증 창을 브라우저를 통해 엽니다.
@@ -93,9 +115,47 @@ class PyTistory:
         process = multiprocessing.Process(target=callback_process, args=(namespace, event))
         process.start()
 
-        time.sleep(2)
+        while not self._is_listening():
+            time.sleep(0.1)
 
-        webbrowser.open_new(TISTORY_AUTHORIZE_URL + TISTORY_AUTHORIZE_PARAMS.format(self.client_id))
+        request_uri = TISTORY_AUTHORIZE_URL + TISTORY_AUTHORIZE_PARAMS.format(self.client_id)
+
+        if not headless_auth:
+            webbrowser.open_new(request_uri)
+        else:
+            try:
+                from selenium import webdriver
+                from selenium.common.exceptions import WebDriverException, NoSuchWindowException
+            except ImportError:
+                raise ImportError('Cannot import selenum.\n' +\
+                    'The headless authentication option need selenium.')
+            try:
+                options = webdriver.ChromeOptions()
+                options.add_argument('headless')
+                driver = webdriver.Chrome(chrome_options=options)
+            except WebDriverException:
+                raise ConfigurationError('Cannot open PhantomJS. Please install PhantomJS.')
+
+            driver.get(request_uri)
+            driver.find_element_by_name('loginId').send_keys(self.tistory_id)
+            driver.find_element_by_name('password').send_keys(self.tistory_password)
+            driver.find_element_by_xpath('//*[@id="authForm"]/fieldset/div/button').click()
+
+            try:
+                current_url = driver.current_url
+            except NoSuchWindowException:
+                current_url = ''
+
+            # 비밀번호 변경하라는 창
+            if current_url.split('?')[0].endswith('outdated'):
+                # 다음에 변경하기
+                driver.find_element_by_xpath('//*[@id="passwordChangeForm"]/fieldset/div/a').click()
+            elif current_url.endswith('auth/login'):
+                process.terminate()
+                process.join()
+                raise ConfigurationError('Email authentication is required to log in to Tistory.')
+
+            driver.quit()
 
         event.wait()
         if hasattr(namespace, 'access_token'):
@@ -108,7 +168,10 @@ class PyTistory:
 
         process.join()
 
-    def configure(self, **kwargs):
+    def configure(self, configure_file_name=None,
+                  client_id=None, secret_key=None,
+                  headless_auth=False,
+                  tistory_id=None, tistory_password=None):
         """Tistory OAuth 2.0 인증을 실행하는 함수입니다.
 
         `Tistory Open API OAuth 인증 <http://www.tistory.com/guide/api/oauth>`_에
@@ -125,30 +188,44 @@ class PyTistory:
         :type client_id: str
         :param secret_key: 티스토리 OAuth를 위한 secret_key 값
         :type secret_key: str
+        :param headless_auth: 사용자가 직접 아이디 패스워드를 입력하지 않고 인증시킵니다. 아이디와 패스워드가 추가로 필요합니다.
+        :type headless_auth: bool
         :raises ConfigurationError: 설정이 불가능할 때 일어납니다.
         """
-        if 'configure_file_name' in kwargs:
+        if configure_file_name is not None:
             # 파일에서 설정읽기
-            self.file_name = kwargs['configure_file_name']
-            config = self._read_configuration_file()
+            self.file_name = configure_file_name
+            config = self._read_configuration_file(headless_auth)
 
             self.client_id = config[CONFIG_SECTION_NAME][CONFIG_CLIENT_ID]
             self.secret_key = config[CONFIG_SECTION_NAME][CONFIG_SECRET_KEY]
-        elif 'client_id' in kwargs and 'secret_key' in kwargs:
-            self.client_id = kwargs['client_id']
-            self.secret_key = kwargs['secret_key']
+            if headless_auth:
+                self.tistory_id = config[CONFIG_SECTION_NAME][CONFIG_TISTORY_ID]
+                self.tistory_password = config[CONFIG_SECTION_NAME][CONFIG_TISTORY_PASSWORD]
+        elif client_id is not None and secret_key is not None:
+            self.client_id = client_id
+            self.secret_key = secret_key
+            if tistory_id is not None and tistory_password is not None:
+                headless_auth = True
+                self.tistory_id = tistory_id
+                self.tistory_password = tistory_password
         else:
-            client_id = os.environ.get('PYTISTORY_CLIENT_ID')
-            secret_key = os.environ.get('PYTISTORY_SECRET_KEY')
+            self.client_id = os.environ.get('PYTISTORY_CLIENT_ID')
+            self.secret_key = os.environ.get('PYTISTORY_SECRET_KEY')
 
-            if client_id and secret_key:
-                self.client_id = client_id
-                self.secret_key = secret_key
-            else:
+            if self.client_id is None or self.secret_key is None:
                 raise ConfigurationError('Cannot configure a PyTistory.')
 
+            if headless_auth:
+                self.tistory_id = os.environ.get('PYTISTORY_TISTORY_ID')
+                self.tistory_password = os.environ.get('PYTISTORY_TISTORY_PASSWORD')
+
+                if self.tistory_id is None or self.tistory_password:
+                    raise ConfigurationError('Cannot configure a PyTistory.\n' +\
+                        'There is no tistory id and password')
+
         # access token 받아오기
-        self._set_access_token()
+        self._set_access_token(headless_auth)
 
         if self.file_name:
             config[CONFIG_SECTION_NAME][CONFIG_ACCESS_TOKEN] = self.access_token
