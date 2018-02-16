4529cb523cac1799537eaee62e48ffd32d43258a
JeongUkJae <JeongUkJae@gmail.com>
2018-02-16 18:08:42 +0900

    테스트 케이스 추가

--- tests/test_003_post.py
+++ tests/test_003_post.py
@@ -1,6 +1,7 @@
 import unittest
 
 from pytistory import PyTistory
+from pytistory.exceptions import NoSpecifiedBlogError
 
 class TestPost(unittest.TestCase):
     def setUp(self):
@@ -10,6 +11,9 @@ class TestPost(unittest.TestCase):
     def test001_최근_게시글_목록(self):
         data = self.pytistory.post.list(blog_name='test-blog-5532')
         self.assertEqual('1', data['item']['page'])
+
+    def test002_최근_게시글_목록_블로그_비_명시(self):
+        self.assertRaises(NoSpecifiedBlogError, self.pytistory.post.list)
 
     def tearDown(self):
         pass
